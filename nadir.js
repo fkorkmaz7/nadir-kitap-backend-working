3da12316df717b34e2ea6b8b043adda8de22ca07
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-11-11 15:15:51 +0300

    Nadir HTML and VUE,JS Connection
    I have completed the html-body codes in the project and vue.js is starting soon.

---
+++ nadir.js
@@ -0,0 +1,127 @@
+// KİTAPLAR KATEGORİSİ NESNELERİ
+
+new Vue({
+    el:'#kitap1',
+    data:{
+        booktype1:'Bilim ve Teknik'
+    }
+})
+
+new Vue({
+    el:'#kitap2',
+    data:{
+        booktype2:'Çizgi Roman'
+    }
+})
+
+new Vue({
+    el:'#kitap3',
+    data:{
+        booktype3:'Çocuk Kitapları'
+    }
+})
+
+new Vue({
+    el:'#kitap4',
+    data:{
+        booktype4:'Din'
+    }
+})
+
+new Vue({
+    el:'#kitap5',
+    data:{
+        booktype5:'Edebiyat'
+    }
+})
+
+new Vue({
+    el:'#kitap6',
+    data:{
+        booktype6:'Ekonomi & İş Dünyası'
+    }
+})
+
+new Vue({
+    el:'#kitap7',
+    data:{
+        booktype7:'Felsefe & Düşünce'
+    }
+})
+
+new Vue({
+    el:'#kitap8',
+    data:{
+        booktype8:'Hukuk'
+    }
+})
+
+new Vue({
+    el:'#kitap9',
+    data:{
+        booktype9:'Osmanlıca'
+    }
+})
+
+new Vue({
+    el:'#kitap10',
+    data:{
+        booktype10:'Referans & Başvuru'
+    }
+})
+
+new Vue({
+    el:'#kitap11',
+    data:{
+        booktype11:'Sağlık'
+    }
+})
+
+new Vue({
+    el:'#kitap12',
+    data:{
+        booktype12:'Sanat'
+    }
+})
+
+new Vue({
+    el:'#kitap13',
+    data:{
+        booktype13:'Sınav ve Ders Kitapları'
+    }
+})
+
+new Vue({
+    el:'#kitap14',
+    data:{
+        booktype14:'Spor'
+    }
+})
+
+new Vue({
+    el:'#kitap15',
+    data:{
+        booktype15:'Tarih'
+    }
+})
+
+new Vue({
+    el:'#kitap16',
+    data:{
+        booktype16:'Toplum & Siyaset'
+    }
+})
+
+new Vue({
+    el:'#kitap17',
+    data:{
+        booktype17:'Diğer & Çeşitli'
+    }
+})
+
+    
+
+
+
+
+
