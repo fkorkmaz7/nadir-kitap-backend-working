26d3196cb2e26d6f5dcc5c83d488bc9429506422
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-12-07 13:34:22 +0300

    vue view design

---
+++ HomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import TutorialVue from '../../../nadirnuxt/components/tutorialnuxt.vue';
+import headercomponentVue from '../components/headercomponent.vue';
+import searchcomponentVue from '../components/searchcomponent.vue';
+import kitaplarkategory from '../components/kitaplarkategory.vue'
+import slidercomponentVue from '../components/slidercomponent.vue';
+import soneklenenlercomponent from '../components/soneklenenlercomponent.vue'
+//import blogyazilaricomponentVue from '../components/blogyazilaricomponent.vue';
+import islemrehbericomponent from '../components/islemrehbericomponent.vue'
+import altcubukcomponent from '../components/altcubukcomponent.vue'
+import yukarıAsagiButonVue from '../components/yukarı-asagi-buton.vue';
+import blogyazilarinuxt from '/../nadirnuxt/components/blogyazilarinuxt.vue'
+//import telifyazisicomponent from '../components/telifyazisicomponent.vue'
+
+
+</script>
+
+<template>
+
+    <div>
+    <headercomponentVue/>
+    <searchcomponentVue/>
+    <kitaplarkategory/>
+    <slidercomponentVue/>
+    <soneklenenlercomponent/>
+    <islemrehbericomponent/>
+    <altcubukcomponent/>
+    <yukarıAsagiButonVue/>
+    <TutorialVue/>
+    <blogyazilarinuxt/>
+    
+    </div>
+    
+
+</template>
+
+
