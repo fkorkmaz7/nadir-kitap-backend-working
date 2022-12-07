5cb10e4660f0cd491b5b77ca726bc7a66cd26e2a
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-12-07 13:33:50 +0300

    nuxt js component design

---
+++ blogyazilarinuxt.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+        <div class="blogdan-son-yazilar"><a style="text-decoration: none;color: darkslategray;" href=""><h4 style="color:#931818" >&#9998; Blog'dan Son Yazılar</h4></a></div>
+<div class="blogdan-yazilar-info"><li style="list-style-type:none;"><a style="text-decoration: none;color: darkslategray;" href="" title="Püzant Akbaş Söyleşisi">“Patrikhanemizin<br/>kütüphanesi dünyada ilk sıradadır.”<br/></a><small><i>Ekim 2022</i> </small></li>
+  <li style="list-style-type:none;"><a style="text-decoration: none;color: darkslategray;" href="" title="Nezih Başgelen Söyleşisi">“Toplumdan kopuk bir arkeolojinin<br/>faydası yok”<br/></a><small><i>Eylül 2022</i> </small></li>
+  <li style="list-style-type:none;"><a style="text-decoration: none;color: darkslategray;" href="" title="Beşir Ayvazoğlu Söyleşisi">“Kütüphane bir nevi<br/>otobiyografidir!”<br/></a><small><i>Ağustos 2022</i> </small></li>
+  <li style="list-style-type:none;"><a style="text-decoration: none;color: darkslategray;" href="" title="Dursun Gürlek Söyleşisi">“Kitap Merakı Bakımından Biraz<br/>Muallim Cevdet’e Benzerim”<br/></a><small><i>Temmuz 2022</i> </small></li>
+  <li style="list-style-type:none;"><a style="text-decoration: none;color: darkslategray;" href="" title="Ercan Topçu Söyleşisi">“Su Müzesi alanında dünyanın en<br/>zengin koleksiyonuyuz!”<br/></a><small><i>Haziran 2022</i> </small></li>
+</div> 
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+   .blogdan-son-yazilar{
+  position:absolute;
+  top: 710px;
+  left: 1270px; 
+  text-decoration: none;
+  font-size: 20px;
+  color: #63b5ea;
+  
+}
+
+.blogdan-yazilar-info{
+  text-decoration: none;
+  position:absolute;
+  top: 780px;
+  left: 1250px;
+  border: 2px dotted #eee4cd;
+  padding: 10px;
+  width: 240px;
+  height: 500px;
+  line-height: 32px;
+  font-size: 16px;
+}
+</style>
