691bf6dcdfccab5975c28e900ed828d3c2efebdf
Furkan korkmaz <104424302+fkorkmaz7@users.noreply.github.com>
2023-01-25 19:06:49 +0300

    product definition and home page definition
    The main page is the main page and the other pages are routed from these pages, and at the same time, the test product objects that I use in the cart application are located on this page.

---
+++ index.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+
+  <blogyazilarinuxt></blogyazilarinuxt>
+  <altcubuknuxt></altcubuknuxt>
+  <headernuxt></headernuxt>
+  <islemrehberinuxt></islemrehberinuxt>
+  <tutorialnuxt></tutorialnuxt>
+  <kitaplarkategorinuxt></kitaplarkategorinuxt>
+  <searchnuxt></searchnuxt>
+  <slidernuxt></slidernuxt>
+  <soneklenenlernuxt></soneklenenlernuxt>
+  <yukariasagibutonnuxt></yukariasagibutonnuxt>
+  
+ <theNavbar></theNavbar>
+ 
+
+ 
+ 
+</div>
+</template>
+
+<script>
+import ShoppingCart from '../components/ShoppingCart.vue'
+import theNavbar from '../components/TheNavbar.vue'
+  export default{
+  components: { theNavbar, ShoppingCart },
+    }
+
+</script>
