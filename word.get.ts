7a23da54da9a2d6b66282fb72caf175e984d69d5
Furkan korkmaz <104424302+fkorkmaz7@users.noreply.github.com>
2023-02-02 03:30:00 +0300

    test and auth server api code

---
+++ word.get.ts
@@ -0,0 +1,11 @@
+// import { word } from "../lib/word";
+
+// export default defineEventHandler((event) => {
+//   return {
+//     word: word, //4 letters
+//   };
+// });
+
+// function defineEventHandler(arg0: (event: any) => { word: string; }) {
+//     throw new Error("Function not implemented.");
+// }
